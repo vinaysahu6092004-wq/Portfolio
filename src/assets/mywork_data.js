b055b6e966a17f0e5edd7f8c36d8f7c45d5ebb59
Vinay Sahu <vinaysahu6092004@gmail.com>
2026-04-13 22:06:48 +0530

    updated website changes

--- src/assets/mywork_data.js
+++ src/assets/mywork_data.js
@@ -1,6 +1,7 @@
 import project1_img from '../assets/project_1.png' 
 import project2_img from '../assets/project_2.png' 
-import project3_img from '../assets/project_3.png' 
+import project3_img from '../assets/project_3.png'
+import project4_img from '../assets/project_4.jpeg'
 const mywork_data= [
   {
     w_no: 1,
@@ -19,6 +20,11 @@ const mywork_data= [
     w_name: "Coding Interview Platform(Under Development)",
     w_img: project3_img,
     w_link: "https://interview-coding-platform-frontend.vercel.app/",
-  },
+  },{
+    w_no: 4,
+    w_name: "Enviroment Monitoring System(ESP32 sense data from the sensor like temperature and Air-Quality and deliver real-time data through MQTT broker)",
+    w_img : project4_img,
+    w_link:"https://esp-32-iot-health-monitoring.vercel.app/"
+  }
 ];
 export default mywork_data;
